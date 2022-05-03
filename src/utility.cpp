f86d93543aa4b6918d7cb4a59352c4cd7b33d183
minjae <kmj0824@postech.ac.kr>
2022-05-03 16:23:56 +0900

    Eliminate the unnecessary part (nearPixel) & minor formatting

--- src/utility.cpp
+++ src/utility.cpp
@@ -61,9 +61,9 @@ using std::to_string;
 using std::string;
 using std::find;
 
-int findIndex(const vector<double> & vec, double item){
+int findIndex(const vector<double> &vec, double item) {
     auto ret = find(vec.begin(), vec.end(), item);
-    if(ret != vec.end())
+    if (ret != vec.end())
         return ret - vec.begin();
     return -1;
 }
@@ -908,11 +908,7 @@ bool circuit::map_move(cell *theCell, int x, int y) {
         pair<bool, pixel *> nearPixel =
                 diamond_search(theCell, myPixel.second->x_pos * wsite,
                                myPixel.second->y_pos * rowHeight);
-        if (nearPixel.first == true) {
-            paint_pixel(theCell, nearPixel.second->x_pos, nearPixel.second->y_pos);
-            // cout << " near found!! " << endl;
-        } else
-            paint_pixel(theCell, myPixel.second->x_pos, myPixel.second->y_pos);
+        paint_pixel(theCell, myPixel.second->x_pos, myPixel.second->y_pos);
         return true;
     } else {
 #ifdef DEBUG
